bcd07ad5941bfdf8775716c42ae12eceb3dabbc4
Heikki Wilenius <hw@iki.fi>
2025-04-12 20:19:46 +0300

    Fix: import PlayerOrAdminRequiredMixin for TournamentCreateView and restore necessary imports.

--- tournament_creator/views/tournament_views.py
+++ tournament_creator/views/tournament_views.py
@@ -1,3 +1,21 @@
+from django.views.generic import ListView, CreateView, DetailView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.http import JsonResponse
+from django.db import models
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_POST
+from ..models.base_models import TournamentChart, Matchup, Pair, TournamentArchetype, Player
+from ..models.scoring import MatchScore, PlayerScore
+from ..models.logging import MatchResultLog
+from ..views.auth import SpectatorAccessMixin, PlayerOrAdminRequiredMixin
+
+class TournamentListView(SpectatorAccessMixin, ListView):
+    model = TournamentChart
+    template_name = 'tournament_creator/tournament_list.html'
+    context_object_name = 'tournaments'
+
 class TournamentCreateView(PlayerOrAdminRequiredMixin, CreateView):
     model = TournamentChart
     template_name = 'tournament_creator/tournament_create.html'
